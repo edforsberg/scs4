f8470b7555cdbf86dec8af0428549633884cc5e0
Erik Forsberg <erikforsberg95@hotmail.com>
2018-12-02 22:41:52 +0100

    started on 4

--- NetworkModels4.m
+++ NetworkModels4.m
@@ -1,5 +1,16 @@
+clear all 
 
+A = csvread('smallWorldExample'); 
+nrTriangles = trace(A^3)/6;
 
- A = []; 
+nrTripplets = 0; 
+for i=1:size(A,1) 
+   ki = sum(A(i,:) == 1); 
+   nrTripplets = nrTripplets + ki*(ki);    
+end
+nrTripplets = nrTripplets/2; 
+
+cClust = nrTriangles*3/nrTripplets; 
+
+disp(cClust); 
  
- nrTri = (A^3)
