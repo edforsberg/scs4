fee43836b67fbc011ad582a44bc803fdc5149267
Erik Forsberg <erikforsberg95@hotmail.com>
2018-12-04 16:53:42 +0100

    a

--- NetworkModels4.m
+++ NetworkModels4.m
@@ -1,12 +1,26 @@
 clear all 
 
 A = csvread('smallWorldExample'); 
-nrTriangles = trace(A^3)/6;
 
+n = size(A,1); 
+r = 100; 
+circStep = 2*pi/n;
+for i = 1:n
+    xPos(i) = cos(circStep*i)*r;
+    yPos(i) = sin(circStep*i)*r;
+end
+
+subplot(1,2,1)
+gplot(A, [xPos', yPos']);
+axis equal
+xlimits([0 r]);
+ylimits([0 r]);
+
+nrTriangles = trace(A^3)/6;
 nrTripplets = 0; 
 for i=1:size(A,1) 
    ki = sum(A(i,:) == 1); 
-   nrTripplets = nrTripplets + ki*(ki);    
+   nrTripplets = nrTripplets + ki*(ki-1);    
 end
 nrTripplets = nrTripplets/2; 
 
