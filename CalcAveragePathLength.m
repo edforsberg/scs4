d1c8d4156cc92a42dfe21d44dc58ea8816ef6a6e
Erik Forsberg <erikforsberg95@hotmail.com>
2018-12-05 18:13:08 +0100

    finnished 5

--- CalcAveragePathLength.m
+++ CalcAveragePathLength.m
@@ -14,8 +14,7 @@ for i = 1:n
     while (connections < n-1)
         pathLengthI  = pathLengthI + sum(newConnections)*steps;
         steps = steps+1;
-        b = find(newConnections);
-        newConnections = A(b,:);        
+        newConnections = A(find(newConnections),:);        
         newConnections = sum(newConnections,1);
         newConnections = (newConnections ~= 0);
         newConnections(i) = 0;
@@ -26,6 +25,7 @@ for i = 1:n
             diameter = steps;
         end
     end
+    pathLengthI = pathLengthI+sum(newConnections)*steps; 
     pathLenghts = pathLenghts + pathLengthI;
 end
 averagePathLength = pathLenghts / (n*(n-1));
--- NetworkModels5.m
+++ NetworkModels5.m
@@ -1,8 +1,6 @@
 clear all
 
-
 A = csvread('smallWorldExample'); 
-A = [0 1 1 0;1 0 0 1;1 0 0 1;0 1 1 0]; 
 plot(graph(A)); 
 
 [averagePathLength, diameter] = CalcAveragePathLength(A)
