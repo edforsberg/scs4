7c3c5f65520de16c3ee80a2c998ca0715c8c737e
Erik Forsberg <erikforsberg95@hotmail.com>
2018-12-05 17:24:23 +0100

    finnised 4

--- CalcClusterC.m
+++ CalcClusterC.m
@@ -1,11 +1,11 @@
 function clusterC = CalcClusterC(A)
 
-nrTriangles = trace(A^3)/6;
+nrTriangles = trace(A^3)/2;
 
 nrTripplets = 0; 
 for i = 1:size(A,1)
     x = sum(A(i,:)); 
-    nrTripplets = nrTripplets + x*(x+1)/2;
+    nrTripplets = nrTripplets + x*(x-1)/2;
 end
 
 clusterC = nrTriangles/nrTripplets; 
--- NetworkModels4.m
+++ NetworkModels4.m
@@ -1,4 +1,19 @@
-clear all 
+clear all
+
+% n = 100; 
+% c = 32; 
+% r = 10; 
+% circStep = 2*pi/n;
+% for i = 1:n
+%     xPos(i) = cos(circStep*i)*r;
+%     yPos(i) = sin(circStep*i)*r;
+% end
+% 
+% A = ones(n);
+% B = tril(A, -c/2-1);
+% C = tril(B, -n+c/2);
+% D = A -(B + B')+(C+C');
+% A =  D -diag(diag(D));
 
 
 A = csvread('smallWorldExample'); 
@@ -18,8 +33,8 @@ xlim([-r r]);
 ylim([-r r]);
 
 clusterC = CalcClusterC(A);
-
 disp(clusterC); 
+title('smallWorldExample'); 
 
 
  
